2b44093671331b323e298cebbccb8e41e88b7a75
Santhi G <101110649+santhi-radiant@users.noreply.github.com>
2022-03-29 11:19:55 +0530

    Update sendmail_test1.php

--- sendmail_test1.php
+++ sendmail_test1.php
@@ -13,7 +13,7 @@ function smtp_mailer($to,$subject, $msg){
 	$mail->IsHTML(true);
 	$mail->CharSet = 'UTF-8';
 	$mail->Username = "apikey";
-	$mail->Password = "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";
+	$mail->Password = "";
 	$mail->SetFrom("santhi.g@radiantcloud.co");
 	$mail->addAttachment("dummy.pdf");
 	$mail->addAttachment("attach.txt");
