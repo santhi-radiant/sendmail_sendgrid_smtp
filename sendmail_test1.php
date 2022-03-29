e42592ea9ed133298be267e65e782a1bef8036e1
santhi-radiant <santhi.g@radiantcloud.co>
2022-03-29 10:43:04 +0530

    first commit

---
+++ sendmail_test1.php
@@ -0,0 +1,34 @@
+<?php 
+$html='<h2>Hello Santhi--success !</h2>';
+echo smtp_mailer('santhi.g@radiantcloud.co','Test mail using SMTP MAILER',$html);
+function smtp_mailer($to,$subject, $msg){
+	require_once("smtp/class.phpmailer.php");
+	$mail = new PHPMailer(); 
+	$mail->IsSMTP(); 
+	$mail->SMTPDebug = 1; 
+	$mail->SMTPAuth = true; 
+	$mail->SMTPSecure = 'TLS'; 
+	$mail->Host = "smtp.sendgrid.net";
+	$mail->Port = 587; 
+	$mail->IsHTML(true);
+	$mail->CharSet = 'UTF-8';
+	$mail->Username = "apikey";
+	$mail->Password = "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";
+	$mail->SetFrom("santhi.g@radiantcloud.co");
+	$mail->addAttachment("dummy.pdf");
+	$mail->addAttachment("attach.txt");
+	$mail->Subject = $subject;
+	$mail->Body =$msg;
+	$mail->AddAddress($to);
+	if(!$mail->Send()){
+		echo 'Mail Not sent';
+		//return 0;
+	}else{
+		echo 'Mail sent successfully';
+		//return 1;
+
+	}
+}
+
+?>
+
